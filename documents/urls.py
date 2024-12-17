80e4a5da6cdcbf8fbd48476e152b44caaff86ce5
Жилин Андрей <152083348+tengenatari@users.noreply.github.com>
2024-12-18 00:04:59 +0500

    123123

--- documents/urls.py
+++ documents/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [path('', views.index, name='index'),
 
                path('reader/<str:instance_id>', views.create_reader, name='book'),
                 path('create/book/<str:instance_id>', views.insert_book, name='book'),
+               path('delete/reader/<str:instance_id>', views.delete_reader, name='book'),
+               path('delete/book/<str:instance_id>', views.delete_book, name='book'),
                ]
--- documents/views.py
+++ documents/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 from documents.forms import ReaderForm, BookForm
 from documents.models import Reader, Book
@@ -7,8 +7,19 @@ from djongo.models.fields import ObjectId, ArrayField
 
 
 def index(request):
+    if request.GET:
+        req = dict(request.GET)
+        clear_req = {}
 
-    return render(request, 'documents/base.html', {'readers': Reader.objects.all()})
+        for key in req:
+            if req[key] != ['']:
+                clear_req[key] = req[key]
+        instance = Reader.objects.filter(**clear_req).all()
+        
+
+    else:
+        instance = Reader.objects.all()
+    return render(request, 'documents/base.html', {'readers': instance})
 
 
 def create_reader(request, instance_id):
@@ -29,24 +40,35 @@ def create_reader(request, instance_id):
         if books:
             for book in books:
                 book_forms.append(BookForm(instance=Book.objects.get(_id=ObjectId(book['_id']))))
-    return render(request, 'documents/create_reader.html', context={'form': form, 'book_form': BookForm(), 'books': book_forms, "instance": instance})
+    return render(request, 'documents/create_reader.html', context={'form': form, 'book_form': BookForm(),
+                                                                    'books': book_forms, "instance": instance})
 
 
-def insert_book(request, instance_id):
-    instance = Reader.objects.get(_id=ObjectId(instance_id))
-    form = BookForm(request.POST).save()
-    if instance.books:
-        instance.books.append(Book.objects.get(_id=ObjectId(form.id)).__dict__)
+def insert_book(request, instance_id, subinstance_id):
+
+    if subinstance_id == "0":
+        instance = Reader.objects.get(_id=ObjectId(instance_id))
+        form = BookForm(request.POST).save()
+        if instance.books:
+            instance.books.append(Book.objects.get(_id=ObjectId(form.id)).__dict__)
+        else:
+            instance.books = [Book.objects.get(_id=ObjectId(form.id)).__dict__]
     else:
-        instance.books = [Book.objects.get(_id=ObjectId(form.id)).__dict__]
+        instance = Book.objects.get(_id=ObjectId(subinstance_id))
+        form = BookForm(request.POST).save()
+        if instance.books:
+            instance.books.append(Book.objects.get(_id=ObjectId(form.id)).__dict__)
+        else:
+            return HttpResponse(404)
     instance.save()
+    return redirect('/user')
 
 
-def update_reader(request):
-    form = ReaderForm()
+def delete_reader(request, instance_id):
+    instance = Reader.objects.get(_id=ObjectId(instance_id))
+    instance.delete()
 
-    if request.method == 'POST':
-        form = ReaderForm(request.POST)
-        form.save(commit=True)
 
-    return render(request, 'documents/create_reader.html', context={'form': form})
+def delete_book(request, instance_id):
+    instance = Book.objects.get(_id=ObjectId(instance_id))
+    instance.delete()
